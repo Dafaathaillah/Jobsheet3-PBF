340a2a357bd594fcb9b6a0a87c621cfdf2a8ccaa
Dafa Bintang Athaillah <Dafabintang76@gmail.com>
2022-03-16 12:29:53 +0800

    Praktikum Modul 4 Bug Praktikum 4

--- src/index.js
+++ src/index.js
@@ -9,17 +9,21 @@ import StateFullComponent from './container/StateFullComponent';
 import ListImage from './component/ListImage';
 import Footer from './component/Footer';
 import Test from './container/Test';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BlogPost from './container/BlogPost/BlogPost'
 
 // ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 // ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
 // ReactDOM.render(<ListImage />, document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Test />, document.getElementById('root'));
+// ReactDOM.render(<Test />, document.getElementById('root'));
 
 //Test Footer and List 
 // ReactDOM.render(<Footer />, document.getElementById('root'));
 // ReactDOM.render(<ListImage />, document.getElementById('root'));
 
+//Test BlogPost
+ ReactDOM.render(<BlogPost />, document.getElementById('content'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
